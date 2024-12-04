01d942baef64e679ed4ad13c59007d1ccffc7e65
Arion <arion@innovazion.ch>
2024-12-04 14:44:08 -0800

    Add new helper scripts

---
+++ install_cuda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Fail on errors
+set -e
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb
+apt update
+apt install -y cuda-toolkit
---
+++ uninstall_cuda.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Fail on errors
+set -e
+
+apt --purge -y remove "*cuda*" "*cublas*" "*cufft*" "*cufile*" "*curand*" "*cusolver*" "*cusparse*" "*gds-tools*" "*npp*" "*nvjpeg*" "nsight*" "*nvvm*"
